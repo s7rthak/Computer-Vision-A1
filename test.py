edc4182df2457455b8b7ec1fca3a5389f55098a9
As4s1n <behera.sarthak3@gmail.com>
2021-09-26 17:10:53 +0530

    illumination

--- test.py
+++ test.py
@@ -9,6 +9,24 @@ eval_end = None
 
 kernel = np.ones((3,3),np.uint8)
 
+def remove_shadows(image):
+    rgb_planes = cv2.split(image)
+
+    result_planes = []
+    result_norm_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        result_planes.append(diff_img)
+        result_norm_planes.append(norm_img)
+
+    # result = cv2.merge(result_planes)
+    result_norm = cv2.merge(result_norm_planes)
+
+    return result_norm
+
 def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
     """Return a sharpened version of the image, using an unsharp mask."""
     blurred = cv2.GaussianBlur(image, kernel_size, sigma)
@@ -32,8 +50,8 @@ def post_process(frame, th = 10):
     # fr = cv2.medianBlur(frame, 3)
     ret, fr = cv2.threshold(frame, th, 255, cv2.THRESH_BINARY)
     fr = cv2.dilate(fr, kernel)
-    fr = cv2.morphologyEx(fr, cv2.MORPH_OPEN, kernel)
-    fr = cv2.morphologyEx(fr, cv2.MORPH_CLOSE, kernel)
+    # fr = cv2.morphologyEx(fr, cv2.MORPH_OPEN, kernel)
+    # fr = cv2.morphologyEx(fr, cv2.MORPH_CLOSE, kernel)
 
     return fr
 
@@ -44,29 +62,38 @@ def background_perform(args,R1,rand_samples_1,boot_strap_1,n_min_1):
     
     frame_name = "in" + str(1).zfill(6) + ".jpg"
     frame1 = cv2.imread(args.inp_path + frame_name)
-    #frame1 = cv2.resize(frame1, (320,240))
+    # frame1 = cv2.resize(frame1, (320,240))
     prvs = pre_process(frame1)
     hsv = np.zeros_like(frame1)
     hsv[...,1] = 255
 
     of = updated_flow.OpticalFlow()
     bg = updated_flow.ModelBackground()
+    backSub_mog = cv2.createBackgroundSubtractorMOG2(history=40,detectShadows=False)
+    backSub_knn = cv2.createBackgroundSubtractorKNN(history=40,detectShadows=False)
 
 
     for i in range(1, FRAMES+1):
         frame_name = "in" + str(i).zfill(6) + ".jpg"
         frame = cv2.imread(args.inp_path + frame_name)
-        #frame = cv2.resize(frame, (320,240))
+        # frame = cv2.resize(frame, (320,240))
         frame2 = frame
         
+        # frame = remove_shadows(frame)
         frame = pre_process(frame)
-        flow = cv2.calcOpticalFlowFarneback(prvs, frame, None, 0.5, 10, 20, 3, 7, 1.5, 0)
-        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-        hsv[...,0] = ang*180/np.pi/2
-        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-        bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
-        bgr = post_process(bgr)
+
+        # cv2.imshow("frame", frame)
+        # keyboard = cv2.waitKey(30)
+        # if keyboard == 'q' or keyboard == 27:
+        #     break
+
+        # flow = cv2.calcOpticalFlowFarneback(prvs, frame, None, 0.5, 10, 20, 3, 7, 1.5, 0)
+        # mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        # hsv[...,0] = ang*180/np.pi/2
+        # hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        # bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        # bgr = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+        # bgr = post_process(bgr)
         
         of.insert_image(frame2)
         flow_magnitude = of.compute_optical_flow()
@@ -77,16 +104,28 @@ def background_perform(args,R1,rand_samples_1,boot_strap_1,n_min_1):
         mask2 = (mask_bg.reshape(mask_bg.shape[0],-1)* mask_of)**(0.9)
         mask_fin = updated_flow.mask2gray(mask2)
         ret, store_fin = cv2.threshold(mask_fin, 25, 255, cv2.THRESH_BINARY)
-        bs.apply(frame, store_fin) 
-        ret, mask_fin = cv2.threshold(mask_fin, 40, 255, cv2.THRESH_BINARY)
+        bs.apply(frame) 
+        ret, mask_fin = cv2.threshold(mask_fin, 45, 255, cv2.THRESH_BINARY)
         res = bs.get('get_image')
+
+        mog = backSub_mog.apply(frame)
+        knn = backSub_knn.apply(frame)
             
         if not res is None:
             mask_fin = mask_fin & res
-            mask_fin = cv2.medianBlur(mask_fin, 3)    
+            mask_fin = cv2.medianBlur(mask_fin, 5)  
+            mask_fin = post_process(mog,10) & post_process(knn,10) & mask_fin
+            # mask_fin = cv2.dilate(mask_fin, (3,3))
+            # mask_fin = cv2.morphologyEx(mask_fin, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
+            mask_fin = cv2.resize(mask_fin, (320,240))
             pred_name = "gt" + str(i).zfill(6) + ".png"
             if i >= eval_start and i <= eval_end:
                 cv2.imwrite(args.out_path + pred_name, mask_fin)
+
+        # pred_name = "gt" + str(i).zfill(6) + ".png"
+        # if i >= eval_start and i <= eval_end:
+        #     ans = cv2.resize(post_process(mog,130) & post_process(knn,250) & mask_fin, (320,240))
+        #     cv2.imwrite(args.out_path + pred_name, ans)
                 
 
 def background_perform2(args,R1,rand_samples_1,boot_strap_1,n_min_1):
